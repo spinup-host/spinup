0ea7364b0720a1cf0ba0cf855c689d7521b3b399
idoko <okokomichaels@outlook.com>
2022-04-24 12:27:18 +0100

    improve filter in get container

--- internal/dockerservice/container.go
+++ internal/dockerservice/container.go
@@ -12,17 +12,10 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/api/types/volume"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/pkg/errors"
-
-	"github.com/spinup-host/spinup/misc"
 )
-
-type Docker struct {
-	Cli *client.Client
-}
 
 // NewDocker returns a Docker struct
 func NewDocker(opts ...client.Opt) (Docker, error) {
@@ -56,39 +49,6 @@ func NewContainer(name string, config container.Config, hostConfig container.Hos
 		HostConfig:    hostConfig,
 		NetworkConfig: networkConfig,
 	}
-}
-
-// GetContainer returns a docker container with the provided name (if any exists).
-// if no match exists, it returns a nil container and a nil error.
-func (d Docker) GetContainer(ctx context.Context, name string) (*Container, error) {
-	listFilters := filters.NewArgs()
-	listFilters.Add("name", name)
-	containers, err := d.Cli.ContainerList(ctx, types.ContainerListOptions{All: true, Filters: listFilters})
-	if err != nil {
-		return &Container{}, fmt.Errorf("error listing containers %w", err)
-	}
-
-	for _, match := range containers {
-		// TODO: name of the container has prefixed with "/"
-		// I have hardcoded here; perhaps there is a better way to handle this
-		if misc.SliceContainsString(match.Names, "/"+name) {
-			data, err := d.Cli.ContainerInspect(ctx, match.ID)
-			if err != nil {
-				return nil, errors.Wrapf(err, "getting data for container %s", match.ID)
-			}
-
-			c := &Container{
-				ID:   match.ID,
-				Name: name,
-				Config: *data.Config,
-				NetworkConfig: network.NetworkingConfig{
-					EndpointsConfig: data.NetworkSettings.Networks,
-				},
-			}
-			return c, nil
-		}
-	}
-	return nil, nil
 }
 
 // Start starts a docker container. If the base image doesn't exist locally, we attempt to pull it from
@@ -219,11 +179,3 @@ func (c *Container) Remove(ctx context.Context, d Docker) error {
 	return d.Cli.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{})
 }
 
-func CreateVolume(ctx context.Context, d Docker, opt volume.VolumeCreateBody) (types.Volume, error) {
-	log.Println("INFO: volume created successfully ", opt.Name)
-	return d.Cli.VolumeCreate(ctx, opt)
-}
-
-func RemoveVolume(ctx context.Context, d Docker, volumeID string) error {
-	return d.Cli.VolumeRemove(ctx, volumeID, true)
-}
